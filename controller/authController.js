f8420aa84d83a2a08225918f23e8f269cb4e3903
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2024-11-12 22:44:10 +0530

    implemented signin api

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,12 @@
-const user = require("../db/models/user");
 const User = require("../db/models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const generateToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 const signUp = async (req, res, next) => {
   const body = req.body;
@@ -20,14 +27,54 @@ const signUp = async (req, res, next) => {
     confirmPassword: body.confirmPassword,
   });
 
-  if (!newUser) {
+  const result = newUser.toJSON();
+
+  delete result.deletedAt;
+  delete result.password;
+
+  result.token = generateToken({
+    id: result.id,
+    email: result.email,
+  });
+
+  if (!result) {
     return res.status(500).json({
       status: "Fail",
       message: "Failed to create the user",
     });
   }
 
-  res.status(201).json(newUser);
+  res.status(201).json(result);
 };
 
-module.exports = { signUp };
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      status: "Fail",
+      message: "Please provide email and password to proceed",
+    });
+  }
+
+  const result = await User.findOne({ where: { email } });
+
+  if (!result || !(await bcrypt.compare(password, result.password))) {
+    return res.status(401).json({
+      status: "Fail",
+      message: "Incorrect email or password",
+    });
+  }
+
+  const token = generateToken({
+    id: result.id,
+    email: result.email,
+  });
+
+  return res.json({
+    status: "Success",
+    token,
+  });
+};
+
+module.exports = { signUp, signIn };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,8 @@
-const { signUp } = require("../controller/authController");
+const { signUp, signIn } = require("../controller/authController");
 
 const router = require("express").Router();
 
 router.route("/signup").post(signUp);
+router.route("/signin").post(signIn);
 
 module.exports = router;
